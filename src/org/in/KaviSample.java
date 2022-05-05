3fcde8669edf2e80e3468d72d1b9359279d10348
kaviyarasu210797 <kaviyarasu210797>
2022-05-05 16:00:26 +0530

    add some few lines in A

--- src/org/in/KaviSample.java
+++ src/org/in/KaviSample.java
@@ -3,9 +3,11 @@ package org.in;
 public class KaviSample {
 
 	static int a = 100;
+	static int b=200;
 
 	public static void main(String[] args) {
 
 		System.out.println(a);
+		System.out.println(b);
 	}
 }
