2978a4b85961d226813c7f3b048257510c2387bb
kaviyarasu210797 <kaviyarasu210797>
2022-05-05 17:04:57 +0530

    c added

--- src/org/in/KaviSample.java
+++ src/org/in/KaviSample.java
@@ -3,9 +3,12 @@ package org.in;
 public class KaviSample {
 
 	static int a = 100;
+	
+	static String name  ="Greens";
 
 	public static void main(String[] args) {
 
 		System.out.println(a);
+		System.out.println(name);
 	}
 }
